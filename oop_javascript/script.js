774cf9474daf15e7cffe5ab42f810f1be07950b5
Fernando Henrique <89784479+FerHe05@users.noreply.github.com>
2023-11-16 14:24:41 -0300

    Update script.js

--- oop_javascript/script.js
+++ oop_javascript/script.js
@@ -1,74 +1,80 @@
-const form = document.querySelector("[data-form]")
-const lists = document.querySelector("[data-lists]")
-const input = document.querySelector("[form-input]")
+// grab all elements 
+const form = document.querySelector("[data-form]");
+const lists = document.querySelector("[data-lists]");
+const input = document.querySelector("[data-input]");
 
-
-
-class Storage{
-    static addTodoStorage(todoArray){
-        let storage = localStorage.setItem("todo", JSON.stringify(todoArray))
-        return storage
+//local Storage
+class Storage {
+    static addTodStorage(todoArr){
+        let storage = localStorage.setItem("todo", JSON.stringify(todoArr));
+        return storage;
     }
 
     static getStorage(){
-        let storage = localStorage.getItem("todo") === null ? [] : JSON.parse(localStorage.getItem("todo"))
+        let storage = localStorage.getItem("todo") === null ? 
+        [] : JSON.parse(localStorage.getItem("todo"));
         return storage
     }
 }
 
-let todoArray = Storage.getStorage()
+// empty array 
+let todoArr = Storage.getStorage();
 
-form.addEventListener("submit", (e) =>{
+// form part 
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let id = Math.random() * 1000000
-    const todo = new Todo(id, input.value)
-    todoArray = [...todoArray, todo];
-    UI.displayData()
-    UI.clearInput()
-    UI.removeTodo()
-    Storage.addTodoStorage(todoArray)
-})
+    let id = Math.random() * 1000000;
+    const todo = new Todo(id, input.value);
+    todoArr = [...todoArr, todo];
+    UI.displayData();
+    UI.clearInput();
+    //add to storage
+    Storage.addTodStorage(todoArr);
+});
 
-class Todo{
+// make object instance 
+class Todo {
     constructor(id, todo){
-        this.id = id
-        this.todo = todo
+        this.id = id;
+        this.todo = todo;
     }
 }
 
+// display the todo in the DOM;
 class UI{
     static displayData(){
-        let displayData = todoArray.map((item) =>{
-            return
-            `
+        let displayData = todoArr.map((item) => {
+            return `
                 <div class="todo">
-                    <p>${item.todo}</p>
-                    <span class= "remove" data-id = ${item.id}> X </span>
+                <p>${item.todo}</p>
+                <span class="remove" data-id = ${item.id}>🗑️</span>
                 </div>
             `
-        })
-        lists.innerHTML = (displayData).join(" ")
+        });
+        lists.innerHTML = (displayData).join(" ");
     }
     static clearInput(){
-        input.value = ""
+        input.value = "";
     }
-
     static removeTodo(){
-        lists.addEventListener("click", (e) =>{
+        lists.addEventListener("click", (e) => {
             if(e.target.classList.contains("remove")){
-                e.target.parentElement.remove()
+                e.target.parentElement.remove();
             }
-            let btnId = e.target.dataset.id
-            UI.removeArrayTodo(btnId)
-        })
+            let btnId = e.target.dataset.id;
+            //remove from array.
+            UI.removeArrayTodo(btnId);
+        });
     }
     static removeArrayTodo(id){
-        todoArray = todoArray.filter((item) => item.id !== + id)
-        Storage.addTodoStorage(todoArray)
+        todoArr = todoArr.filter((item) => item.id !== +id);
+        Storage.addTodStorage(todoArr);
     }
 }
 
-window.addEventListener("DOMContentLoaded", () =>{
+//once the browser is loaded
+window.addEventListener("DOMContentLoaded", () => {
     UI.displayData();
+    //remove from the dom
     UI.removeTodo();
-})
+});
