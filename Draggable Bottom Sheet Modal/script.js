c23d04acca91b22627799e251511a2439a666840
Fernando Henrique <89784479+FerHe05@users.noreply.github.com>
2023-11-20 08:54:52 -0300

    Add files via upload

--- Draggable Bottom Sheet Modal/script.js
+++ Draggable Bottom Sheet Modal/script.js
@@ -1,56 +1,64 @@
-const showModalBtn = document.querySelector(".show-modal")
-const bottomSheet = document.querySelector(".bottom-sheet")
-const sheetOverlay = bottomSheet.querySelector(".sheet-overlay")
-const sheetContent= bottomSheet.querySelector(".content")
-const dragIcon = bottomSheet.querySelector(".drag-icon")
+// Select DOM elements
+const showModalBtn = document.querySelector(".show-modal");
+const bottomSheet = document.querySelector(".bottom-sheet");
+const sheetOverlay = bottomSheet.querySelector(".sheet-overlay");
+const sheetContent = bottomSheet.querySelector(".content");
+const dragIcon = bottomSheet.querySelector(".drag-icon");
 
-let isDragging = false, startY, startHeight
+// Global variables for tracking drag events
+let isDragging = false, startY, startHeight;
 
-const showBottomSheet = () =>{
-    bottomSheet.classList.add("show")
-    document.body.style.overflowY = "hiddens"
-    updateSheetHeight(50)
+// Show the bottom sheet, hide body vertical scrollbar, and call updateSheetHeight
+const showBottomSheet = () => {
+    bottomSheet.classList.add("show");
+    document.body.style.overflowY = "hidden";
+    updateSheetHeight(50);
 }
 
-const hideBottomSheet = () =>{
-    bottomSheet.classList.remove("show")
+const updateSheetHeight = (height) => {
+    sheetContent.style.height = `${height}vh`; //updates the height of the sheet content
+    // Toggles the fullscreen class to bottomSheet if the height is equal to 100
+    bottomSheet.classList.toggle("fullscreen", height === 100);
 }
 
-const updateSheetHeight = (height)=>{
-    sheetContent.style.height = `${height}vh`
-    bottomSheet.classList.toggle("fullscreen", height === 100)
+// Hide the bottom sheet and show body vertical scrollbar
+const hideBottomSheet = () => {
+    bottomSheet.classList.remove("show");
+    document.body.style.overflowY = "auto";
 }
 
-
-const dragStart = (e) =>{
-    isDragging = true
-    startY = e.pageY || e.touches?.[0].pageY
-    startHeight = parseInt(sheetContent.computedStyleMap.height)
-    bottomSheet.classList.add("dragging")
+// Sets initial drag position, sheetContent height and add dragging class to the bottom sheet
+const dragStart = (e) => {
+    isDragging = true;
+    startY = e.pageY || e.touches?.[0].pageY;
+    startHeight = parseInt(sheetContent.style.height);
+    bottomSheet.classList.add("dragging");
 }
 
-const dragging = (e) =>{
-    if(!isDragging) return
-    const delta = startY - (e.pageY || e.touches?.[0].pageY)
-    const newHeight = startHeight + delta / window.innerHeight * 100
-    updateSheetHeight(newHeight)
+// Calculates the new height for the sheet content and call the updateSheetHeight function
+const dragging = (e) => {
+    if(!isDragging) return;
+    const delta = startY - (e.pageY || e.touches?.[0].pageY);
+    const newHeight = startHeight + delta / window.innerHeight * 100;
+    updateSheetHeight(newHeight);
 }
 
-const dragStop = () =>{
-    isDragging = false
-    bottomSheet.classList.remove("dragging")
-    const sheetHeight = parseInt(sheetContent.style.height)
-    sheetHeight < 25 ? hideBottomSheet() : sheetHeight > 75 ? updateSheetHeight(100) :  updateSheetHeight(50)
+// Determines whether to hide, set to fullscreen, or set to default 
+// height based on the current height of the sheet content
+const dragStop = () => {
+    isDragging = false;
+    bottomSheet.classList.remove("dragging");
+    const sheetHeight = parseInt(sheetContent.style.height);
+    sheetHeight < 25 ? hideBottomSheet() : sheetHeight > 75 ? updateSheetHeight(100) : updateSheetHeight(50);
 }
 
-document.addEventListener("mouseup", dragStop)
-dragIcon.addEventListener("mousemove", dragStart)
-dragIcon.addEventListener("mousemove", dragging)
+dragIcon.addEventListener("mousedown", dragStart);
+document.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
 
+dragIcon.addEventListener("touchstart", dragStart);
+document.addEventListener("touchmove", dragging);
+document.addEventListener("touchend", dragStop);
 
-document.addEventListener("touchend", dragStop)
-dragIcon.addEventListener("touchstart", dragStart)
-dragIcon.addEventListener("touchmove", dragging)
-
-showModalBtn.addEventListener("click", showBottomSheet)
-sheetOverlay.addEventListener("click", hideBottomSheet)
+sheetOverlay.addEventListener("click", hideBottomSheet);
+showModalBtn.addEventListener("click", showBottomSheet);
